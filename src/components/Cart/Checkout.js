c3eb20da21bb2c22b192c710f6ff621a8d6febd2
amelsemic <semicamel@hotmail.com>
2022-07-09 20:10:47 +0200

    order inputs validation

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -61,21 +61,28 @@ const Checkout = (props) => {
     ctx.items.length = 0;
   };
 
+
+  let nameValidity = formInputsValidity.name  ? classes.control : classes.invalid;
+  let cityValidity = formInputsValidity.city  ? classes.control : classes.invalid;
+  let postalCodeValidity = formInputsValidity.postalCode  ? classes.control : classes.invalid;
+  let streetValidity = formInputsValidity.street  ? classes.control : classes.invalid;
+  
+
   return (
     <form className={classes.form} onSubmit={confirmHandler}>
-      <div className={classes.control}>
+      <div className={nameValidity}>
         <label htmlFor="name">Your Name</label>
         <input type="text" id="name" ref={nameInputRef} />
       </div>
-      <div className={classes.control}>
+      <div className={streetValidity}>
         <label htmlFor="street">Street</label>
         <input type="text" id="street" ref={streetInputRef} />
       </div>
-      <div className={classes.control}>
+      <div className={postalCodeValidity}>
         <label htmlFor="postal">Postal Code</label>
         <input type="text" id="postal" ref={postalCodeInputRef} />
       </div>
-      <div className={classes.control}>
+      <div className={cityValidity}>
         <label htmlFor="city">City</label>
         <input type="text" id="city" ref={cityInputRef} />
       </div>
